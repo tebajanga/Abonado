92ef33bde6480602afad26b3e0f992b6386bb552
Timothy Anthony <anthony.timothy90@gmail.com>
2018-08-28 22:54:32 +0300

    feat(Abonado) Abonado module with translation

---
+++ language/pt_br.lang.php
@@ -0,0 +1,25 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = array(
+	'ModuleName' => 'Nome Módulo',
+	'SINGLE_ModuleName' => 'Nome Módulo',
+	'ModuleName ID' => 'ID Nome Módulo',
+
+	'LBL_CUSTOM_INFORMATION' => 'Imformação Customizada',
+	'LBL_MODULEBLOCK_INFORMATION' => 'Informação Bloco Módulo',
+	'LBL_DESCRIPTION_INFORMATION' => 'Descrição',
+
+	'LBL_ABONADO' => 'Assinante',
+	'LBL_UBICACION' => 'Localização',
+	'LBL_PANEL' => 'Painel',
+	'LBL_OPERARIO' => 'Operador',
+);
+?>
